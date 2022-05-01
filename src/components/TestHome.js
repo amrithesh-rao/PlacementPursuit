adb6798d1c63259e5642944d47d7dcf666f22c32
Vishal Rajkumar Naik <vishalrajkumar13232@gmail.com>
2022-05-01 18:42:06 +0530

    creating test home page v1

--- src/components/TestHome.js
+++ src/components/TestHome.js
@@ -1,15 +1,15 @@
 import React from "react";
-import NavBar from "../components/NavBar";
+import NavBar from "./NavBar";
 import Footbar from "./Footbar";
 import { Link, useNavigate } from "react-router-dom";
-
+import TopicCard from "./TopicCard";
 
 const TestHome = () => {
     let navigate = useNavigate();
   return (
     <>
     <NavBar />
-    
+        <TopicCard title="Data Structures" />
     <Footbar />
     </>
   );
