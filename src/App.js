caa1f085a1bffb1ce1d0fd19fd12387509f6c7f9
Vishal Rajkumar Naik <vishalrajkumar13232@gmail.com>
2022-05-06 19:44:27 +0530

    created level page and some part of quiz

--- src/App.js
+++ src/App.js
@@ -5,6 +5,9 @@ import Home from "./components/Home";
 import Login from "./components/Login";
 import Signup from "./components/Signup";
 import Reset from "./components/Reset";
+import Test from "./components/Test";
+import Quiz from "./components/Quiz";
+import TestInner from "./components/TestInner";
 import ProtectedRoute from "./components/ProtectedRoute";
 import { UserAuthContextProvider } from "./context/UserAuthContext";
 import Practice from "./components/Practice";
@@ -15,6 +18,9 @@ function App() {
     <Container className="p-0">
       <UserAuthContextProvider>
         <Routes>
+          <Route path="/" element={<Login />} />
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/reset" element={<Reset />} />
           <Route
             path="/home"
             element={
@@ -31,12 +37,33 @@ function App() {
               </ProtectedRoute>
                 } 
           />
-
-          <Route path="/" element={<Login />} />
-          <Route path="/signup" element={<Signup />} />
-          <Route path="/reset" element={<Reset />} />
-
-          {/* <Route path="/test" element={<TestHome />} /> */}
+          
+          <Route 
+            path="/test" 
+            element={
+              <ProtectedRoute>
+                <Test />
+              </ProtectedRoute>
+            } 
+          />
+          <Route
+           path="/test/:id" 
+           element={
+            <ProtectedRoute>
+              <TestInner />
+              </ProtectedRoute>
+                } 
+          />
+          <Route
+           path="/test/:tid/:lid" 
+           element={
+            <ProtectedRoute>
+              <Quiz />
+              </ProtectedRoute>
+                } 
+          />
+          
+          
           {/* <Route path="/feedback" element={<FeedbackHome />} /> */}
         </Routes>
       </UserAuthContextProvider>
---
+++ src/components/Quiz.js
@@ -0,0 +1,48 @@
+import React, { useEffect, useState } from "react";
+import NavBar from "./NavBar";
+import Footbar from "./Footbar";
+import { db } from "../firebase.js";
+import {collection,getDocs} from "firebase/firestore"
+import { useParams } from "react-router-dom";
+import NumberBox from "./NumberBox";
+import QuizCard from "./QuizCard";
+
+export default function Quiz() {
+  const {lid,tid} = useParams();
+  const [quiz, setQuiz] = useState([]);
+  const [qno, setQno] = useState(5);
+  
+  useEffect(  () => {
+    try{
+    const collectionRef = collection(db,"quiz_questions",tid,"levels",lid,"questions");
+    getDocs(collectionRef)
+      .then( snapshot => {
+      setQuiz(snapshot.docs.map(doc => ({
+          data: doc.data()
+      })))
+  })
+  }
+  catch(e){
+    console.log(e);
+}  
+    
+    
+
+  }, [tid,lid]);
+  console.log(quiz);
+  // console.log(values);
+   console.log();
+  return (
+    <>
+      <NavBar />
+      {
+        quiz?.map(questions => (
+            <NumberBox questionNo={quiz.indexOf(questions)+1} />
+            
+      
+      ))}
+       <QuizCard questionNo={qno} question={quiz[qno].data.question} options={quiz[qno].data.options} />
+      <Footbar /> 
+    </>
+  );
+}
