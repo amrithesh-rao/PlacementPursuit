3ed2f3666fd5f03637bb0de6d6e870bd718d44c6
Adesha H P <adeshaholagodu@gmail.com>
2022-05-09 21:54:49 +0530

    feedback adding

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,24 +1,16 @@
 import FeedbackLogo from '../img/preview.jpg';
 const SearchBar = () => (
-    <div><img clasName="container"
+    <div><img className="feedback-img"
     src={FeedbackLogo}
     alt="First slide"
-    width={500} height={350}/>
+    height={350}/>
 
-<div class="container">
-      <div id="content">
-        <form class='form-inline'>
-          <div class="input-group">
-            <input type='text' id='search' class="form-control search-form" placeholder="Search Feedback"/>
-            <span class="input-group-btn" width={200}>
-              <button id="search-this"type="button" class="pull-right btn btn-default search-btn">
-                  <i class="fa fa-search">Search</i>
-              </button>
-            </span>
-          </div>
-        </form>
+    <div>
+      <form className='box'>
+        <input type='text' className='search' placeholder='Search Feedback' required/>
+        <button type='button' className='search-btn'>Search</button>
+      </form>
     </div>
-  </div>
 </div>
 );
 
