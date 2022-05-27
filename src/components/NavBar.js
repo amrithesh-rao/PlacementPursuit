005d43ed44bb25cde953cc170f91b59d19c0defe
Vishal Rajkumar Naik <vishalrajkumar13232@gmail.com>
2022-05-27 17:48:13 +0530

    edit name

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,13 +1,18 @@
-import React from 'react';
+import React, { useRef, useState ,useEffect} from "react";
 import { Navbar , Container, Nav,NavDropdown } from 'react-bootstrap';
 import logo from '../img/logo.png';
 import { useUserAuth } from "../context/UserAuthContext";
+import { Button, Modal, Form } from "react-bootstrap";
 import { useNavigate } from "react-router-dom";
+import {  updateProfile } from 'firebase/auth';
 
 export default function NavBar() {
     const { user, logOut } = useUserAuth();
     const navigate = useNavigate();
-
+    const uName = useRef("");
+  const [show, setShow] = useState(false);
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
     const handleLogout = async () => {
         try {
           await logOut();
@@ -16,7 +21,19 @@ export default function NavBar() {
           console.log(error.message);
         }
       };
+      async function saveName(e){
+
+        e.preventDefault();
+        const {name}=e.target.elements
+        uName.current=name.value;
+        handleClose();
+        updateProfile(user,{
+          displayName: uName.current
+          });
+        window.location.reload(); 
+      }
   return (
+    <>
     <Navbar collapse OnSelect expand="lg"  variant="light">
   <Container className='navBar'>
   <Navbar.Brand onClick={() => navigate("/home")}><img className='size-m' src={logo} alt='logo' /></Navbar.Brand>
@@ -32,11 +49,33 @@ export default function NavBar() {
     <img src={user.photoURL} className="profile-pic" alt="profile"/>
       <NavDropdown title={user.displayName} id="collasible-nav-dropdown">
 
+        <NavDropdown.Item onClick={ handleShow }>Edit Name</NavDropdown.Item>
         <NavDropdown.Item onClick={ handleLogout }>Log Out</NavDropdown.Item>
       </NavDropdown>
     </Nav>
   </Navbar.Collapse>
   </Container>
 </Navbar>
+<Modal show={show} onHide={handleClose} centered={true} backdrop="static">
+      <Form onSubmit={saveName}>
+        <Modal.Header closeButton>
+          <Modal.Title>Setup your name</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+        Your name will be used to save Feedback!!
+
+          
+          <Form.Control type="text" placeholder="Enter your name" name='name' />
+         
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="primary" type="submit">
+            Save
+          </Button>
+          
+        </Modal.Footer>
+        </Form>
+      </Modal>
+      </>
   )
 }
